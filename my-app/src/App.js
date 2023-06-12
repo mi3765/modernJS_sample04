806a04d4dcf3b75942a9294d94b5e52cc6fec839
mi3765 <chips1225@outlook.jp>
2023-06-12 21:15:17 +0900

    buttonを押すとinputTextが切り替わる機能を実装

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import { React, useState } from 'react';
+import { ChildArea } from './ChildArea';
 
 export default function App() {
+  const [ text, setText] = useState("");
+  const [ open, setOpen ] = useState(false);
+  
+  const onChangeText = (e) => {
+    setText(e.target.value);
+  };
+
+  const onClickOpen = () => {
+    setOpen(!open);
+  };
+
   return (
     <>
-    
+      <input value={text} onChange={onChangeText}/>
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open}></ChildArea>
     </>
   );
 };
